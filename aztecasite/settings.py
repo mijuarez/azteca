7fd8a51831d1f1913e5b4d30868640958359e397
mjuareztello <mjuareztello@gmail.com>
2022-04-06 16:06:08 +0000

    Nueva Aplicación Web Azteca Prospectos

--- aztecasite/settings.py
+++ aztecasite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'America/Mexico_City'
 
